8c2aefa9e165c2327bc97127e53d8e34b1c7edf3
Edgard Levy <edgardlevy@gmail.com>
2020-05-04 23:12:16 -0300

    finish challenge

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
+import api from './services/api';
 import "./styles.css";
 
+
 function App() {
+
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data)
+    })
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', { title: `Desafio 02 ${Date.now()}`, owner: 'Edgard Levy' })
+    setRepositories([...repositories,response.data])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`)
+    setRepositories(repositories.filter(repo=>repo.id !== id))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
+        {repositories.map(repo => {
+          return (<li key={repo.title}>
+            {repo.title}
+            <button onClick={() => handleRemoveRepository(repo.id)}>
+              Remover
           </button>
-        </li>
+          </li>)
+        })}
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
